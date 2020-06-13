27bac6ffd44d934bf89d347bfc00a42960499f0a
Samantha Goodman <slgoodman89@gmail.com>
2020-06-12 22:04:48 -0400

    completed task one of closure and started task 2

--- challenges/closure.js
+++ challenges/closure.js
@@ -1,5 +1,5 @@
 // ==== Closures ==== 
-
+console.log('**** CLOSURES ****');
 /* Task 1: Study the code below and explain in your own words why nested function can access the variable internal. */
 
 
@@ -17,8 +17,13 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
-
+console.log( '## task one: explanation ## \n',
+  'The nested function can access the variable internal because they are in the same scope. The varibable internal is declared in the myFunction (functional) scope which is the block scope that nestedFunction is within.'
+);
 
 /* Task 2: Counter */
-
+console.log('## task two: counter ##')
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+function summation() {
+  
+}
