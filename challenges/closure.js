9a109e331b98f9ef857434ae9ebf4d28695fd8e5
Samantha Goodman <slgoodman89@gmail.com>
2020-06-12 23:05:55 -0400

    completed interview questions

--- challenges/closure.js
+++ challenges/closure.js
@@ -25,6 +25,9 @@ console.log( '## task one: explanation ## \n',
 console.log('## task two: counter ##')
 /* Create a function called `summation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
 function summation(num) {
-  console.log('FINISH LATER')
+  let arr = [];
+  for (let i=1; i < num + 1; i++) {
+    arr.push(i);
+  }  return arr.reduce((acc, val) => acc + val)
 }
-summation();
+console.log('SUMMATION OF 4:', summation(4));
